d60560d41251392a62d0f96e3e03b3b3ea0d438b
MaulanaHamzah <maulanahamzah20092004@gmail.com>
2023-09-27 09:35:22 +0700

    Fitur login sistem peminjaman

---
+++ FiturLogin.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class FiturLogin {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int username, password;
+        System.out.println("Masukkan username anda = ");
+        username = input.nextInt();
+        System.out.println("Masukkan password anda = ");
+        password = input.nextInt();
+
+        if (username == 123 && password == 123) {
+            System.out.println("Login anda berhasil");
+        } 
+        else {
+            System.out.println("Login anda gagal");
+        }
+    }
+}
