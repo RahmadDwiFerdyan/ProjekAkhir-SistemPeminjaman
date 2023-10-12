24f48e37f5126009d82d9f86e7bb23935c2a72a2
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2023-10-12 19:38:08 +0700

    v2

--- cobaperulangan.java
+++ cobaperulangan.java
@@ -7,25 +7,26 @@ public class cobaperulangan {
     int i = 1;
 
     String username, password, konfirmasi;
+    System.out.println("          LOGIN           ");
     System.out.print("Masukkan username  : ");
     username = input.nextLine();
     System.out.print("Masukkan password  : ");
     password = input.nextLine();
 
- if (username.equalsIgnoreCase("maha") && password.equalsIgnoreCase("siswa")) {
-    System.out.println("\n>>> Login berhasil <<<");
+ if (username.equalsIgnoreCase("polinema") && password.equalsIgnoreCase("jti")) {
+    System.out.println("\n>>> Login berhasil <<<\n");
  } else { 
     
     do {
         System.out.println("\n### Login gagal ###");
-        System.out.print("Apakah anda ingin mencoba lagi (y/t)?");
+        System.out.print("Apakah anda ingin mencoba lagi (y/t)? ");
         konfirmasi = input.nextLine();
             if (konfirmasi.equalsIgnoreCase("y")) {
             System.out.print("\nMasukkan username  : ");
             username = input.nextLine();
             System.out.print("Masukkan password  : ");
             password = input.nextLine();
-                if (username.equalsIgnoreCase("maha") && password.equalsIgnoreCase("siswa")) {
+                if (username.equalsIgnoreCase("polinema") && password.equalsIgnoreCase("jti")) {
                 System.out.println("\n>>> Login berhasil <<<\n");
                 break;
                 } else {
@@ -37,10 +38,6 @@ public class cobaperulangan {
 
     } while (i==1); 
 }
-    
-
-
-
 
 
 }
