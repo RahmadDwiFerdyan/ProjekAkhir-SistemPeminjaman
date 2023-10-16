400562bd47bae5286a621fece17f6b75160e963d
MaulanaHamzah <144118963+MaulanaHamzah@users.noreply.github.com>
2023-10-16 18:17:16 +0700

    Fitur Peminjaman Buku

---
+++ Fitur Peminjaman/FiturPeminjaman.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class FiturPeminjaman {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String judulBuku, penulis;
+        int tahunTerbit, jumlahBuku;
+
+        System.out.println("===== Fitur Peminjaman Buku =====");
+        System.out.print("Masukkan jumlah buku yang akan dipinjam = ");
+        jumlahBuku = input.nextInt();
+        input.nextLine();
+        System.out.println("====================================================");
+
+        int i = 0;
+
+        while (i < jumlahBuku) {
+            System.out.print("Masukkan judul buku yang akan dipinjam = ");
+            judulBuku = input.nextLine();
+            System.out.print("Masukkan penulis buku = ");
+            penulis = input.nextLine();
+            System.out.print("Masukkan tahun terbit buku = ");
+            tahunTerbit = input.nextInt();
+            input.nextLine();
+            System.out.println("====================================================");
+            i++;
+        }
+        System.out.println("Terimakasih sudah menggunakan program ini");
+    }
+}
