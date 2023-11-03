b444b2bb84a1b8447310f6235b8118809c7841b9
MaulanaHamzah <144118963+MaulanaHamzah@users.noreply.github.com>
2023-11-03 15:05:09 +0700

    Update FiturKatalogBuku.java

--- Fitur Manajemen Katalog Buku/FiturKatalogBuku.java
+++ Fitur Manajemen Katalog Buku/FiturKatalogBuku.java
@@ -3,29 +3,25 @@ import java.util.Scanner;
 public class FiturKatalogBuku {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jumlah buku: ");
+        int jmlBuku = input.nextInt();
+        String[][] buku = new String [jmlBuku][4];
 
-        String judulBuku, penulis;
-        int tahunTerbit, jumlahBuku;
-
-        System.out.println("===== Fitur Manajemen Katalog Buku =====");
-        System.out.print("Masukkan jumlah buku yang akan ditambahkan = ");
-        jumlahBuku = input.nextInt();
-        input.nextLine();
-        System.out.println("====================================================");
-
-        int i = 0;
-
-        while (i < jumlahBuku) {
-            System.out.print("Masukkan judul buku yang akan ditambah = ");
-            judulBuku = input.nextLine();
-            System.out.print("Masukkan penulis buku = ");
-            penulis = input.nextLine();
-            System.out.print("Masukkan tahun terbit buku = ");
-            tahunTerbit = input.nextInt();
-            input.nextLine();
-            System.out.println("====================================================");
-            i++;
+        for (int i = 0; i < jmlBuku; i++) {
+            System.out.println("Masukkan data buku " + (i+1) + " : ");
+            System.out.print("Judul Buku: ");
+            buku[i][0] = input.next();
+            System.out.print("Penulis Buku: ");
+            buku[i][1] = input.next();
+            System.out.print("Tahun Terbit: ");
+            buku[i][2] = input.next();
+            System.out.print("Nomor Induk Buku: ");
+            buku[i][3] = input.next();
         }
-        System.out.println("Terimakasih sudah menggunakan program ini");
+        System.out.println("Data buku: ");
+        System.out.println("Buku" + "\t" + "Judul Buku" + "\t" + "Penulis Buku" + "\t" + " Tahun Terbit" + "\t" + "Nomor Induk Buku");
+        for (int i = 0; i < jmlBuku; i++) {
+            System.out.println((i+1) + "\t" + buku[i][0] + "\t" + buku[i][1] + "\t" + buku[i][2] + "\t" + buku[i][3]);
+        }
     }
 }
