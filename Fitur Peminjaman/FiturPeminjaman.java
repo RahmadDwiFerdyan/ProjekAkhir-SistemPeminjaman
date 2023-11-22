baf942263ef1f09a77ddac5f3405373dfada068f
MaulanaHamzah <144118963+MaulanaHamzah@users.noreply.github.com>
2023-11-22 10:30:25 +0700

    Update FiturPeminjaman.java

--- Fitur Peminjaman/FiturPeminjaman.java
+++ Fitur Peminjaman/FiturPeminjaman.java
@@ -1,33 +1,32 @@
 import java.util.Scanner;
 public class FiturPeminjaman {
+    static Scanner input = new Scanner(System.in);
+    static String[] judulBuku;
+    static String[] penulis;
+    static int[] tahunTerbit;
+    static int jumlahBuku;
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        String judulBuku, penulis;
-        int tahunTerbit, jumlahBuku, tanggalPeminjaman;
-
         System.out.println("===== Fitur Peminjaman Buku =====");
         System.out.print("Masukkan jumlah buku yang akan dipinjam = ");
         jumlahBuku = input.nextInt();
         input.nextLine();
+        judulBuku = new String[jumlahBuku];
+        penulis = new String[jumlahBuku];
+        tahunTerbit = new int[jumlahBuku];
         System.out.println("====================================================");
-
-        int i = 1;
-
-        while (i <= jumlahBuku) {
-            System.out.println("Informasi buku ke-" + i + "=");
-            System.out.print("Masukkan judul buku yang akan dipinjam = ");
-            judulBuku = input.nextLine();
-            System.out.print("Masukkan penulis buku = ");
-            penulis = input.nextLine();
-            System.out.print("Masukkan tahun terbit buku = ");
-            tahunTerbit = input.nextInt();
-            System.out.print("Masukkan tanggal peminjaman = ");
-            tanggalPeminjaman = input.nextInt();
-            input.nextLine();
-            System.out.println("====================================================");
-            i++;
-        }
+        dataBuku();
         System.out.println("Terimakasih sudah menggunakan program ini");
+    }
+    static void dataBuku() {
+        for (int i = 0; i < jumlahBuku; i++) {
+            System.out.println("Buku ke- " + (i+1));
+            System.out.print("Masukkan judul buku yang akan dipinjam: ");
+            judulBuku[i] = input.nextLine();
+            System.out.print("Masukkan penulis buku: ");
+            penulis[i] = input.nextLine();
+            System.out.print("Masukkan tahun terbit buku: ");
+            tahunTerbit[i] = input.nextInt();
+            input.nextLine();
+        }
     }
 }
