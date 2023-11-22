04526190d2ce725b2a8d908d7212668acec89361
MaulanaHamzah <144118963+MaulanaHamzah@users.noreply.github.com>
2023-11-23 05:45:38 +0700

    Modifikasi fitur login terbaru

--- Fitur Login/FiturLogin.java
+++ Fitur Login/FiturLogin.java
@@ -2,37 +2,31 @@ import java.util.Scanner;
 public class FiturLogin {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String username, password, konfirmasi;
-    System.out.println("          LOGIN           ");
-    System.out.print("Masukkan username  : ");
-    username = input.nextLine();
-    System.out.print("Masukkan password  : ");
-    password = input.nextLine();
+        String[] arrayID = {"hamzah02", "Febri11", "Ferdyan20"};
+        String[] arrayPW = {"polinema02", "polinema11", "polinema20"};
 
- if (username.equals("polinema") && password.equals("jti")) {
-    System.out.println("\n>>> Login berhasil <<<\n");
- } else { 
-    
-    do {
-        System.out.println("\n### Login gagal ###");
-        System.out.print("Apakah anda ingin mencoba lagi (y/t)? ");
-        konfirmasi = input.nextLine();
-            if (konfirmasi.equalsIgnoreCase("y")) {
-            System.out.print("\nMasukkan username  : ");
-            username = input.nextLine();
-            System.out.print("Masukkan password  : ");
-            password = input.nextLine();
-                if (username.equalsIgnoreCase("polinema") && password.equalsIgnoreCase("jti")) {
-                System.out.println("\n>>> Login berhasil <<<\n");
-                break;
-                } else {
-                continue;
-                }
-            } else {
-                break;
-            } 
+        System.out.println("Selamat Datang didalam |SISTEM PEMINJAMAN BUKU|");
+        System.out.print("Masukkan ID User: ");
+        String userID = input.nextLine();
+        System.out.print("Masukkan Password: ");
+        char[] passwordArray = input.nextLine().toCharArray();
+        String userPW = new String(passwordArray);
 
-    } while (konfirmasi != "t"); 
-}
+        // Memeriksa ID pengguna dan password
+        if (fiturLogin(userID, userPW, arrayID, arrayPW)) {
+            System.out.println("Login Berhasil !!");
+        } else {
+            System.out.println("Login gagal. ID User atau Password salah.");
+        }
+    }
+
+    // Fungsi untuk memeriksa ID pengguna dan password
+    private static boolean fiturLogin(String userID, String password, String[] arrayID, String[] arrayPW) {
+        for (int i = 0; i < arrayID.length; i++) {
+            if (userID.equals(arrayID[i]) && password.equals(arrayPW[i])) {
+                return true;
+            }
+        }
+        return false;
     }
 }
